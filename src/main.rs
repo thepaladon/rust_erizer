376dac0a9955be64c3578f372a9739e0cb0d0ab0
thepaladon <angel@doncho.net>
2023-01-26 15:00:27 +0100

    colors and clear screen with color

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,18 @@ pub fn edge_fun(p : Vec2, v0 : Vec2, v1 : Vec2) -> f32 {
     v0_p.x * v0_v1.y - v0_p.y * v0_v1.x
 }
 
+//Courtesy of Kamen
+//pub fn to_argb8<T: Into<u32> + Copy>(a: T, r: T, g: T, b: T) -> u32
+//{
+//    let mut argb = a.into();
+//
+//    argb = (argb << 8) + r.into();
+//    argb = (argb << 8) + g.into();
+//    argb = (argb << 8) + b.into();
+//
+//    argb
+//}
+
 fn to_argb8(a : u8, r : u8, g : u8, b : u8) -> u32 {
 
     let mut argb : u32 = a as u32; 
@@ -33,6 +45,7 @@ fn to_argb8(a : u8, r : u8, g : u8, b : u8) -> u32 {
     argb
 
 }
+
 
 fn render_triangle(color : Vec3, x : Vec2, y : Vec2, z : Vec2, p : Vec2) -> Vec3 {
 
@@ -68,14 +81,22 @@ fn main() {
         panic!("{}", e);
     });
 
+    let red = Vec3::new(255.0, 0.0, 0.0);
+    let green = Vec3::new(255.0, 0.0, 0.0);
+    let blue = Vec3::new(0.0, 0.0, 255.0);
+    let white = Vec3::new(255.0, 255.0, 255.0);
+    let gray = Vec3::new(128.0, 128.0, 128.0);
+    let black = Vec3::new(0.0, 0.0, 0.0);
+
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(0)));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let now = Instant::now();
 
-        // Clear Screen
-        buffer.fill(0);
+        // Clear Screen (doesn't work currently because of the other iteration)
+        //let clear_color = to_argb8(255, white.x as u8, white.y as u8, white.z as u8);
+        //buffer.fill(0xffffffff);
 
         for i in 0..buffer.len() {
             let x = i as f32 % WIDTH as f32;
@@ -84,12 +105,9 @@ fn main() {
             let p = Vec2::new(x, y);
 
             let mut fc = Vec3::new(0.0, 0.0, 0.0); //final color 
-
-            let color0 = Vec3::new(0.0, 0.0, 255.0);
-            let color1 = Vec3::new(255.0, 0.0, 0.0);
-
-            fc += render_triangle(color0, v0, v1, v2, p);
-            fc += render_triangle(color1, v0, v3, v1, p);
+            
+            fc += render_triangle(white, v0, v1, v2, p);
+            fc += render_triangle(gray, v0, v3, v1, p);
           
             buffer[i] = to_argb8(255, fc.x as u8, fc.y as u8, fc.z as u8);
         }
