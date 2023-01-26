16148e111c25d9e816ca286c35569b4014bff888
thepaladon <angel@doncho.net>
2023-01-26 14:30:13 +0100

    triangle func

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate minifb;
 use std::time::Instant;
 use minifb::{Key, Window, WindowOptions};
 use glam::Vec2;
+use glam::Vec3;
 
-const WIDTH: usize = 800;
-const HEIGHT: usize = 512;
+const WIDTH: usize = 1024;
+const HEIGHT: usize = 800;
 
 pub fn edge_fun(p : Vec2, v0 : Vec2, v1 : Vec2) -> f32 {
     let v0_p = p - v0;
@@ -24,12 +25,29 @@ fn to_argb8(a : u8, r : u8, g : u8, b : u8) -> u32 {
 
 }
 
+fn render_triangle(color : Vec3, x : Vec2, y : Vec2, z : Vec2, p : Vec2) -> Vec3 {
+
+        let mut fc = Vec3::new( 0.0, 0.0, 0.0 );
+        
+        // clock wise check
+        let area0 = edge_fun(p, x, y);
+        let area1 = edge_fun(p, y, z);
+        let area2 = edge_fun(p, z, x);
+
+        if area0 < 0.0 && area1 < 0.0 && area2 < 0.0 { 
+            fc = color;
+        }
+
+        fc
+}
+
 fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     let v0 = Vec2::new(600.0, 100.0 );
     let v1 = Vec2::new(400.0, 300.0 );
     let v2 = Vec2::new(200.0, 100.0 );
+    let v3 = Vec2::new(600.0, 300.0 );
 
     let mut window = Window::new(
         "Hello Triangle",
@@ -49,9 +67,9 @@ fn main() {
         let now = Instant::now();
 
         //clear function basically
-        //for i in buffer.iter_mut() {
-        //    *i = to_argb8(0, 0, 0, 0); // write something more funny here!
-        //}
+        for i in buffer.iter_mut() {
+            *i = to_argb8(0, 0, 0, 0); // write something more funny here!
+        }
 
         for i in 0..buffer.len() {
             let x = i as f32 % WIDTH as f32;
@@ -59,15 +77,15 @@ fn main() {
 
             let p = Vec2::new(x, y);
 
-            let area0 = edge_fun(p, v0, v1);
-            let area1 = edge_fun(p, v1, v2);
-            let area2 = edge_fun(p, v2, v0);
+            let mut fc = Vec3::new(0.0, 0.0, 0.0); //final color 
 
-            let r = if area0 > 0.0 { 255 } else { 0 };
-            let g = if area1 > 0.0 { 255 } else { 0 };
-            let b = if area2 > 0.0 { 255 } else { 0 };
+            let  color0 = Vec3::new(0.0, 0.0, 255.0);
+            let  color1 = Vec3::new(255.0, 0.0, 0.0);
 
-            buffer[i] = to_argb8(0, r, g, b);
+            fc += render_triangle(color0, v0, v1, v2, p);
+            fc += render_triangle(color1, v0, v3, v1, p);
+          
+            buffer[i] = to_argb8(255, fc.x as u8, fc.y as u8, fc.z as u8);
         }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
